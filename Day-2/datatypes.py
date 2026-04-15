b563f58b6cfbbab8a8acafd42e027d3a17bcb646
Abhiram Teki <62495637+abhiram-teki@users.noreply.github.com>
2026-04-15 13:10:23 +0530

    Add files via upload

---
+++ Day-2/datatypes.py
@@ -0,0 +1,22 @@
+myVar="valid"
+myVar1="valid"
+my_Var="valid"
+print("Cannot use keywords or special symbols")
+a=1
+b=12.3
+c=12+3j
+#used in machine learning
+print(a, b, c)
+str="Welcome to Codegnan"
+print(str)
+l=[1,2,3,4]
+print(l)
+l2=[1,2,3,3,4]
+print(l2)
+l3=[1,2.1,'three',4+1j]
+print(l3)
+s={1,2,2,3,3,3,4,4,4,4}
+print(s)
+d={'Name':'Abhiram', 'Grad':'2025', 'Course':'PFS', 'Batch':'52'}
+print(d)
+
