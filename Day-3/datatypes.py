4339004a659175d504ffb94963d5e87e301f255c
Abhiram Teki <62495637+abhiram-teki@users.noreply.github.com>
2026-04-16 12:26:55 +0530

    Add files via upload

---
+++ Day-3/datatypes.py
@@ -0,0 +1,55 @@
+#Integer
+product_q = 3
+order_id = 10939289878
+print(product_q, order_id)
+
+#Float
+product_price=749.9
+discount_percentage=35.5
+average_rating=4.5
+
+#Complex
+z=5+2j
+
+#String
+customer_name="Robert Robertson"
+delivery_address="Hyderabad, Telangana"
+product_category="Electronics"
+print(customer_name, delivery_address, product_category)
+
+#List
+cart=["Bluetooth Speaker", "Television", "IEMs"]
+top_categories=["Electronics", "Fashion", "Home"]
+
+#Tuple
+warehouse_location=(12.9898, 77.7938)
+product_dimensions=(12.5, 1.23, 2.2)
+print(warehouse_location)
+
+#Set
+available_sizes={"S", "M", "L"}
+brands={"Nike", "Adidas", "Puma", "Nike"}
+print(brands)
+
+#FrozenSet(immutable)
+frozen_t= frozenset(["Sale", "Limited Edition", "Trending"])
+
+#Dictionary
+product_details={
+    "name":"IEMs"
+    "brand":"Truthear"
+    "price":"6700"
+    "rating":"4.5"
+    }
+print(product_details)
+
+#Boolean
+is_logged_in= True
+is_payment_succesful=False
+
+#None
+tracking_number= None
+delivery_partner = None
+
+
+
